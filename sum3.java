59db210cbbfce4d47fcd840b75ef2718703de7ba
D G Janaki <dgjanakidg@gmail.com>
2026-09-10 20:48:09 +0530

    Adding 3sum problem

---
+++ sum3.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class sum3 {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(sum(nums));
+    }    
+
+    static List<List<Integer>> sum(int[] nums){
+     List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+     int n =nums.length;
+     for (int i = 0; i < n-2; i++) {
+        int j=i+1;
+        int k = n-1;
+        
+        
+        if(i>0 && nums[i]==nums[i-1]) continue;
+
+        while(j<k){
+            int sum = nums[i] +nums[j] +nums[k];
+            if(sum==0){
+                res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                while (j<k && nums[j] == nums[j+1]) {
+                    j++;                    
+                }
+                 while (j<k && nums[k] == nums[k-1]) {
+                    k--;                    
+                }
+                j++;
+                k--;
+            }
+            else if(sum>0){
+                k--;
+            }
+            else{
+                j++;
+            }
+        }
+     }
+     return res;
+    }
+}
